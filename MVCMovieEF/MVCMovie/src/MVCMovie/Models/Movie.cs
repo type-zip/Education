aba69a4fa83c89ca285d62569e49291e6720e556
Igor P <get4him@yandex.ru>
2017-02-06 17:50:43 +0300

    First commit

---
+++ MVCMovieEF/MVCMovie/src/MVCMovie/Models/Movie.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Required]
+        [StringLength(30)]
+        public string Genre { get; set; }
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Required]
+        [StringLength(5)]
+        public string Rating { get; set; }
+    }
+}
---
+++ MVCMovieEF/MVCMovie/src/MVCMovie/Models/SeedData.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MVCMovie.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCMovie.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ApplicationDbContext(serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
+            {
+
+                if(context.Movie.Any())
+                {
+                    return;
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "When Harry Met Sally",
+                        ReleaseDate = DateTime.Parse("2016-01-11"),
+                        Genre = "Romantic Comedy",
+                        Price = 250.0M,
+                        Rating = "R"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ghostbusters",
+                        ReleaseDate = DateTime.Parse("2001-01-12"),
+                        Genre = "Fiction",
+                        Price = 300.0M,
+                        Rating = "D"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Wind That Shakes A Barley",
+                        ReleaseDate = DateTime.Parse("1986-02-03"),
+                        Genre = "Documentary",
+                        Price = 450.0M,
+                        Rating = "C"
+                    },
+
+                    new Movie
+                    {
+                        Title = "My Little Armalie",
+                        ReleaseDate = DateTime.Parse("1950-01-01"),
+                        Genre = "Jam",
+                        Price = 750000.0M,
+                        Rating = "A"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
