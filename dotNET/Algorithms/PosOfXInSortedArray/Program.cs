5922dfce1ec7a3d926b1ba25ebb829073cc4047b
Igor P <get4him@yandex.ru>
2017-03-07 18:24:19 +0300

    +

--- dotNET/Algorithms/PosOfXInSortedArray/Program.cs
+++ dotNET/Algorithms/PosOfXInSortedArray/Program.cs
@@ -6,27 +6,71 @@ namespace PosOfXInSortedArray
     {
         static void Main(string[] args)
         {
-            int[] values = new int[25];
+            int[] values = new int[30000000];
 
-            var r = new Random();
+            var r = new Random(156878161);
 
             for (int i = 0; i < values.Length; i++)
-                values[i] = r.Next(0, 1000);
+                values[i] = r.Next(0, 2000);
 
             Array.Sort(values);
-            PrintArray(values);
+            //PrintArray(values);
 
 
             int Xvalue = values[r.Next(0, values.Length)];
 
             Console.WriteLine($"X is: {Xvalue}");
 
+            var watch = System.Diagnostics.Stopwatch.StartNew();
             int result = Solution(values, Xvalue);
+            watch.Stop();
+            Console.WriteLine($"Binary loop search runtime: {watch.ElapsedMilliseconds} ms.");
+
+            watch = System.Diagnostics.Stopwatch.StartNew();
             int resultRec = SolutionRecursive(values, Xvalue, 0, values.Length - 1);
+            watch.Stop();
+            Console.WriteLine($"Binary recursive search runtime: {watch.ElapsedMilliseconds} ms.");
+
+            watch = System.Diagnostics.Stopwatch.StartNew();
+            int resultInt = SolutionInterpolationSearch(values, Xvalue);
+            watch.Stop();
+            Console.WriteLine($"Interpolation search runtime: {watch.ElapsedMilliseconds} ms.");
 
             Console.WriteLine($"The position of X is: {result}");
             Console.WriteLine($"The position of X is (found recursively): {resultRec}");
             Console.ReadLine();
+        }
+
+        static int SolutionInterpolationSearch(int[] sortedArray, int x)
+        {
+
+
+            if (sortedArray[0] == x)
+                return 0;
+
+            if (sortedArray[sortedArray.Length - 1] == x)
+                return sortedArray.Length - 1;
+
+            int lowerBorder = 0;
+            int upperBorder = sortedArray.Length - 1;
+            int middle = lowerBorder + ( ((x - sortedArray[lowerBorder]) * (upperBorder - lowerBorder)) / (sortedArray[upperBorder] - sortedArray[lowerBorder]) );
+
+            while (lowerBorder <= upperBorder)
+            {
+                middle = lowerBorder + (((x - sortedArray[lowerBorder]) * (upperBorder - lowerBorder)) / (sortedArray[upperBorder] - sortedArray[lowerBorder]));
+                Console.WriteLine($"{middle} = {lowerBorder} + ((({x} - {sortedArray[lowerBorder]}) * ({upperBorder} - {lowerBorder})) / ({sortedArray[upperBorder]} - {sortedArray[lowerBorder]}))");
+
+                if (x == sortedArray[middle])
+                    return middle + 1;
+
+                if (x < sortedArray[middle])
+                    upperBorder = middle - 1;
+
+                if (x > sortedArray[middle])
+                    lowerBorder = middle + 1;
+            }
+
+            return -1;
         }
 
         static int Solution(int[] sortedArray, int x)
